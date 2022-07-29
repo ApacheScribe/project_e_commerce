292e4f8d7065e8f4f60f95bebae2199dcf8709dc
Joe Mwa <joseph.mwangi01@gmail.com>
2022-07-29 19:50:57 +0300

    init

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,7 +35,10 @@ const PrimarySearchAppBar = ({ totalItems }) => {
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
+            {/* <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js */}
+            <span style={{fontFamily: "CandyScript", fontSize: "2em"}}>
+            SparePap ECommerce concept
+            </span>
           </Typography>
           <div className={classes.grow} />
           {location.pathname === '/' && (
